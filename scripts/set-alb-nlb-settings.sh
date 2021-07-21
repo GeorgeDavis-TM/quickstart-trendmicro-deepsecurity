ac358f720d49a4d9fcdbe0f11162b6fccc38f82c
GeorgeDavis-TM <george_davis@trendmicro.com>
2021-07-21 03:10:15 -0400

    Adding new files to support the ALB-NLB deployment model

---
+++ scripts/set-alb-nlb-settings.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+##set-lb-settings <dsmuser> <dsmpass> <managerfqdn> <consoleport> <hbport>
+user=$1
+pass=$2
+albfqdn=$3
+consoleport=$4
+heartbeatport=$5
+manager=localhost:${4}
+nlbfqdn=$6
+
+
+
+SID=`curl -k -H "Content-Type: application/json" -X POST "https://${manager}/rest/authentication/login/primary" -d '{"dsCredentials":{"userName":"'${user}'","password":"'${pass}'"}}'`
+
+curl -k -v -H "Content-Type: text/xml;charset=UTF-8" -H 'SOAPAction: "systemSettingSet"' "https://${manager}/webservice/Manager" -d \
+'<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:Manager">'\
+'<soapenv:Header/>'\
+'<soapenv:Body>'\
+'<urn:systemSettingSet>'\
+'<urn:editableSettings>'\
+'<urn:settingKey>CONFIGURATION_SYSTEMLOADBALANCERHEARTBEATHOSTNAME</urn:settingKey>'\
+'<urn:settingUnit>NONE</urn:settingUnit>'\
+'<urn:settingValue>'${nlbfqdn}'</urn:settingValue>'\
+'</urn:editableSettings>'\
+'<urn:editableSettings>'\
+'<urn:settingKey>CONFIGURATION_SYSTEMLOADBALANCERHEARTBEATPORT</urn:settingKey>'\
+'<urn:settingUnit>PORT</urn:settingUnit>'\
+'<urn:settingValue>'${heartbeatport}'</urn:settingValue>'\
+'</urn:editableSettings>'\
+'<urn:editableSettings>'\
+'<urn:settingKey>CONFIGURATION_SYSTEMLOADBALANCERMANAGERHOSTNAME</urn:settingKey>'\
+'<urn:settingUnit>NONE</urn:settingUnit>'\
+'<urn:settingValue>'${albfqdn}'</urn:settingValue>'\
+'</urn:editableSettings>'\
+'<urn:editableSettings>'\
+'<urn:settingKey>CONFIGURATION_SYSTEMLOADBALANCERMANAGERPORT</urn:settingKey>'\
+'<urn:settingUnit>PORT</urn:settingUnit>'\
+'<urn:settingValue>'${consoleport}'</urn:settingValue>'\
+'</urn:editableSettings>'\
+'<urn:editableSettings>'\
+'<urn:settingKey>CONFIGURATION_SYSTEMLOADBALANCERRELAYHOSTNAME</urn:settingKey>'\
+'<urn:settingUnit>NONE</urn:settingUnit>'\
+'<urn:settingValue>'${albfqdn}'</urn:settingValue>'\
+'</urn:editableSettings>'\
+'<urn:sID>'${SID}'</urn:sID>'\
+'</urn:systemSettingSet>'\
+'</soapenv:Body>'\
+'</soapenv:Envelope'\>
+
+
